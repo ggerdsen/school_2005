1a1ebf066174d73bdad7a97dbb33d826e830fcee
Garrett Gerdsen <garrett.gerdsen@gmail.com>
2020-05-06 13:53:52 -0600

    commit after third iteration

--- lib/school.rb
+++ lib/school.rb
@@ -26,4 +26,15 @@ class School
     int_end_time = int_time + @hours
     return int_end_time.to_s << ":00"
   end
+
+  def is_full_time?
+    if @hours > 4
+      return true
+    end
+  end
+
+  def standard_student_names
+    @names.map(&:capitalize)
+  end
+
 end
