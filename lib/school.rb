a3d13c1576e9bc66b47c8ca51857dd4325d9276b
Garrett Gerdsen <garrett.gerdsen@gmail.com>
2020-05-06 13:35:57 -0600

    initial commit

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,29 @@
+require 'pry'
+class School
+  attr_reader :start_time, :hours, :names
 
+  def initialize(start_time, hours)
+    @start_time = start_time
+    @hours = hours
+    @names = []
+  end
+
+  def hours_in_school_day
+    @hours
+  end
+
+  def add_student_name(name)
+    @names << name
+  end
+
+  def student_names
+    @names
+  end
+
+  def end_time
+    # binding.pry
+    int_time = @start_time.to_i
+    int_end_time = int_time + @hours
+    return int_end_time.to_s << ":00"
+  end
+end
